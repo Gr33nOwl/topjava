71ac25eed68e49c9a5db1253519a661d781f91c5
Osipenko Dmitrii <gr33n@yandex.ru>
2021-11-10 10:34:37 +0300

    My_HW05_Optional

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -25,7 +25,7 @@ import static org.slf4j.LoggerFactory.getLogger;
   public abstract class AbstractServiceTest {
     private static final Logger log = getLogger("result");
 
-    private static final StringBuilder results = new StringBuilder();
+    private static StringBuilder results = new StringBuilder();
 
     @Rule
     // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
@@ -45,6 +45,7 @@ import static org.slf4j.LoggerFactory.getLogger;
                 "\n---------------------------------" +
                 results +
                 "\n---------------------------------");
+        results = new StringBuilder();
     }
 
 
